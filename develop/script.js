8c6e099221f797b443bca2dd79a33fcef622352d
Code 41 <washingtonbenton@gmail.com>
2022-07-01 12:56:31 -0400

    updating

--- develop/script.js
+++ develop/script.js
@@ -1,3 +1,16 @@
+var timeEl = document.querySelector('.time');
+var scoreEl = document.querySelector('#score');
+var questionEl = document.querySelector('.question');
+var buttonsEl = document.querySelector('.btn');
+var choiceaEl = document.querySelector('#choicea');
+var choicebEl = document.querySelector('#choiceb');
+var choicecEl = document.querySelector('#choicec');
+var choicedEl = document.querySelector('#choiced');
+var startEl = document.querySelector('#start');
+
+var secondsLeft = 60;
+var highScore = 0;
+
 var codingQuiz = {
     question1: '1. Which programming language is known as the foundation or bones of a website?',
     choices1: ['HTML','CSS','Javascript','API'],
@@ -16,20 +29,35 @@ var codingQuiz = {
     answer4: 'API',
 };
 
-var display = document.querySelector('.display');
-var start = document.querySelector('.start-button');
-var timeEl = document.querySelector('.time');
-var secondsLeft = 60;
 
-function startTime() {
+
+function takeQuiz() {
+    // start button disappears
+startEl.setAttribute('style', 'visibility: hidden');
+    // Timer begins when user clicks start button
     var timerInterval = setInterval(function() {
         secondsLeft--;
-        timeEl.textContent = secondsLeft + 'seconds left till quiz ends.';
-
-        if(secondsleft === 0) {
+        timeEl.textContent = secondsLeft + ' seconds left till quiz ends.';
+// supposed to stop when hit 0
+        if (secondsleft === 0) {
             clearInterval(timerInterval);
         }
     }, 1000);
+// // Question 1 appears. 
+// If right buttton clicked, +50 pts and go to next question
+// If wrong button clicked, +0 pts and go to next question
+questionEl.textContent = codingQuiz.question1;
+if (choiceaEl.clicked == true) {
+    questionEl.textContent = codingQuiz.question2;
+};
+// if(document.getElementById('choicea').clicked == true) {
+//     scoreEl.textContent = highScore + 50;
+//     questionEl.textContent = codingQuiz.question2
+// else
+//     scoreEl.textContent = highScore + 0;
+//     questionEl.textContent = codingQuiz.question2
+// };
+
 }
 
-start.addEventListener("click", startTime)
+start.addEventListener("click", takeQuiz)
