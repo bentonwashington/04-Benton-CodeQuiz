bbd5834141ab9d3e7b36302b4afdc31409f304ac
Code 41 <washingtonbenton@gmail.com>
2022-07-22 13:21:28 -0400

    late commit, coudlnt figure out how to go to next'

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,7 @@
 var timeEl = document.querySelector('.time');
 var scoreEl = document.querySelector('#score');
 var questionEl = document.querySelector('.question');
-var buttonsEl = document.querySelector('.btn');
+var buttonsEl = document.querySelector('#buttons');
 var choiceaEl = document.querySelector('#choicea');
 var choicebEl = document.querySelector('#choiceb');
 var choicecEl = document.querySelector('#choicec');
@@ -11,53 +11,73 @@ var startEl = document.querySelector('#start');
 var secondsLeft = 60;
 var highScore = 0;
 
-var codingQuiz = {
-    question1: '1. Which programming language is known as the foundation or bones of a website?',
-    choices1: ['HTML','CSS','Javascript','API'],
-    answer1: 'HTML',
+var questionIndex = 0
 
-    question2: '2. Which programming language is known as the "skin" or styling of a website?',
-    choices2: ['HTML','CSS','Javascript','API'],
-    answer2: 'CSS',
+var codingQuiz = [{
+    question: ' 1. Which programming language is known as the foundation or bones of a website?',
+    choices: ['HTML','CSS','Javascript','API'],
+    answer: 'HTML',
+},{
 
-    question3: '3. Which programming language is known as the "brains" or logic of a website?',
-    choices3: ['HTML','CSS','Javascript','API'],
-    answer3: 'Javascript',
+    question: ' 2. Which programming language is known as the "skin" or styling of a website?',
+    choices: ['HTML','CSS','Javascript','API'],
+    answer: 'CSS',
+},{
+    question: ' 3. Which programming language is known as the "brains" or logic of a website?',
+    choices: ['HTML','CSS','Javascript','API'],
+    answer: 'Javascript',
+},{
+    question: ' 4. Which programming language is a supplement to shorten the "Javascript" programming language?',
+    choices: ['HTML','CSS','Javascript','API'],
+    answer: 'API',
+}];
 
-    question4: '4. Which programming language is a supplement to shorten the "Javascript" programming language?',
-    choices4: ['HTML','CSS','Javascript','API'],
-    answer4: 'API',
-};
+function next (event) {
+    console.log(event)
+    if (event.target.textContent === codingQuiz[questionIndex].answer){
+        scoreEl.textContent = highScore += 50;
+        questionIndex ++;
+        questionRender();
+    }
+    else {
+        questionIndex ++;
+        questionRender();
+    }
+    if (questionIndex = 4){
+        funtion 
+        
+    }
+}
+
 
 
 
 function takeQuiz() {
-    // start button disappears
+    // Start button disappears
 startEl.setAttribute('style', 'visibility: hidden');
     // Timer begins when user clicks start button
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft + ' seconds left till quiz ends.';
-// supposed to stop when hit 0
-        if (secondsleft === 0) {
+        scoreEl.textContent = highScore ;
+        if (secondsLeft === 0) {
             clearInterval(timerInterval);
         }
     }, 1000);
 // // Question 1 appears. 
 // If right buttton clicked, +50 pts and go to next question
-// If wrong button clicked, +0 pts and go to next question
-questionEl.textContent = codingQuiz.question1;
-if (choiceaEl.clicked == true) {
-    questionEl.textContent = codingQuiz.question2;
-};
-// if(document.getElementById('choicea').clicked == true) {
-//     scoreEl.textContent = highScore + 50;
-//     questionEl.textContent = codingQuiz.question2
-// else
-//     scoreEl.textContent = highScore + 0;
-//     questionEl.textContent = codingQuiz.question2
-// };
-
+// If wrong button clicked, go to next question
+questionRender();
 }
 
-start.addEventListener("click", takeQuiz)
+function questionRender(){
+    questionEl.textContent = codingQuiz[questionIndex].question;
+    choiceaEl.textContent = codingQuiz[questionIndex].choices[0];
+    choicebEl.textContent = codingQuiz[questionIndex].choices[1];
+    choicecEl.textContent = codingQuiz[questionIndex].choices[2];
+    choicedEl.textContent = codingQuiz[questionIndex].choices[3];
+}
+
+start.addEventListener("click", takeQuiz);
+
+buttonsEl.addEventListener("click", next);
